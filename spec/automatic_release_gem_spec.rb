73fe5e263fd95d9cb6fde8c0e895140555fdac2e
Marcelo Toledo <marcelotoledo5000@gmail.com>
2025-06-25 18:35:54 -0300

    Update automatic_release_gem_spec.rb

--- spec/automatic_release_gem_spec.rb
+++ spec/automatic_release_gem_spec.rb
@@ -3,7 +3,7 @@
 require 'spec_helper'
 
 RSpec.describe AutomaticReleaseGem do
-  subject(:my_method) { described_class.usefull_method? }
+  subject(:my_method) { described_class.useful_method? }
 
   it 'has a version number' do
     expect(AutomaticReleaseGem::VERSION).not_to be_nil
